473d1503e81c3ca2c9fa91870489f01c471ad255
Raunak <reachtoraunak@gmail.com>
2022-02-11 22:41:05 +0530

    [Update] Index grid page responsive :calling:

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -5,39 +5,46 @@ import Link from 'next/link'
 
 const PostCard = ({ post }) => {
   return (
-    <div className="h-100 card-bordered card card-compact mb-4 lg:card-normal">
+    <div className="card-bordered card card-compact mb-4 lg:card-normal">
       <figure>
-        <img src={post.featuredImage.url} alt={post.title} />
-        <div className="flex">
-          <div className="card flex-grow place-items-end">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="mr-2 inline h-6 w-6 text-secondary"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-              />
-            </svg>
-          </div>
-          <span className="mx-2">
-            {moment(post.createdAt).format('MMM DD, YYYY')}
-          </span>
-        </div>
+        <img
+          src={post.featuredImage.url}
+          alt={post.title}
+          className="h-48 object-cover"
+        />
       </figure>
+
+      <div className="absolute top-0 right-0 flex border-l border-b  bg-base-100">
+        <div className=" place-items-end">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mr-2 inline h-6 w-6 text-secondary"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+            />
+          </svg>
+        </div>
+        <span className="mx-2">
+          {moment(post.createdAt).format('MMM DD, YYYY')}
+        </span>
+      </div>
+
       <div className="card-body">
         <h1 className="card-title mb-2 text-xl font-bold tracking-tight text-gray-900 transition duration-700 hover:text-blue-600 dark:text-white">
           <Link href={`/post/${post.slug}`}>{post.title}</Link>
         </h1>
-        <p className="font-normal text-gray-600 dark:text-gray-400">
+        <p className="text-base font-normal dark:text-gray-400">
           {post.excerpt.substring(0, 100)}...
         </p>
-        <div className="card-actions mt-4">
+
+        <div className="card-actions">
           <button className="btn btn-ghost">
             Read More
             <svg
@@ -57,17 +64,18 @@ const PostCard = ({ post }) => {
         </div>
 
         {post.authors.map((author) => (
-          <div key={author.id} className="mt-2 flex-grow place-items-end">
-            <div className="flex items-start justify-end lg:mb-0 lg:w-auto">
+          <div key={author.id} className="mt-2 flex-grow items-end">
+            <div className="flex items-center justify-start lg:mb-0 lg:w-auto">
               <img
                 src={author.photo.url}
                 alt={author.name}
-                className="align-middle"
+                className="mask mask-squircle align-middle"
                 height="30px"
                 width="30px"
               />
-
-              <p className="link mx-2 text-lg font-bold">{author.name}</p>
+              <p className="link-hover link-secondary link mx-2 hover:text-primary-content">
+                {author.name}
+              </p>
             </div>
           </div>
         ))}
--- pages/index.jsx
+++ pages/index.jsx
@@ -12,22 +12,34 @@ export default function Home({ posts }) {
   console.log(posts)
   return (
     // <div className="mt-4 w-full rounded bg-gradient-to-r from-blue-400/10 via-indigo-500/10 to-green-500/10 p-6">
-    <div className="mt-4 w-full rounded p-6">
-      <div className="container mx-auto mb-8 px-10">
-        <div className="grid grid-cols-1 gap-12 md:grid-cols-12 ">
+    // <div className="mt-4 p-6">
+    //   <div className="container mx-auto mb-8 px-10">
+    //     <div className="grid grid-cols-1 gap-12 lg:grid-cols-12 ">
+    //       {posts.map((post, index) => (
+    //         <div className="col-span-1 lg:col-span-8">
+    //           <PostCard key={index} post={post.node} />
+    //         </div>
+    //       ))}
+    //       <div className="col-span-1 lg:col-span-4">
+    //         <div className="relative top-8 lg:sticky">
+    //           <PostWidget />
+    //           <Categories />
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    <div className="flex items-center justify-evenly">
+      {/* Grid System */}
+      <div className="grid w-full max-w-screen-lg basis-3/4 gap-8 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-3 md:grid-cols-1 lg:col-span-2 lg:grid-cols-2 xl:grid-cols-3">
           {posts.map((post, index) => (
-            <div className="col-span-1 md:col-span-6 lg:col-span-4">
-              <PostCard key={index} post={post.node} />
-              <PostCard key={index} post={post.node} />
-
-              <PostCard key={index} post={post.node} />
-            </div>
+            <PostCard key={index} post={post.node} />
           ))}
-          <div className="col-span-1 md:col-span-6 lg:col-span-4">
-            <div className="relative top-8 lg:sticky">
-              <PostWidget />
-              <Categories />
-            </div>
+        </div>
+        <div className="col-span-1 basis-1/4 lg:col-span-1">
+          <div className="relative top-8 md:sticky lg:sticky">
+            <PostWidget />
+            <Categories />
           </div>
         </div>
       </div>
